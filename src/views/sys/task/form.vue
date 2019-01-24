ec5d41db26b1bedd35429090e9623bcb0538287b
hejiawang <952327407@qq.com>
2019-01-24 16:36:17 +0800

    init

---
+++ src/views/sys/task/form.vue
@@ -0,0 +1,38 @@
+<template>
+  <Modal v-model="isShow" :title="title" :loading="loading" @on-ok="ok" @on-cancel="cancel" @on-visible-change="visibleChange" >
+
+  </Modal>
+</template>
+<script>
+export default {
+  name: 'SysMenu_Form',
+  props: {
+    value: {type: Boolean, default: false, required: true},
+    type: {type: String, default: 'raise', required: true}
+  },
+  computed: {
+    title () {
+      let titleAry = {
+        'modify': '编辑任务信息',
+        'raise': '新增任务信息'
+      }
+      return titleAry[this.type]
+    }
+  },
+  watch: {
+    value (val) { this.isShow = val },
+    isShow (val) { this.$emit('input', val) }
+  },
+  data () {
+    return {
+      loading: true,
+      isShow: false
+    }
+  },
+  methods: {
+    ok () {},
+    cancel () {},
+    visibleChange (isOpen) {}
+  }
+}
+</script>
