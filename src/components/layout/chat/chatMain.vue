f794706495a61aa90dd1793d1c384f52b050d70d
hejiawang <952327407@qq.com>
2019-02-23 09:25:53 +0800

    init

--- src/components/layout/chat/chatMain.vue
+++ src/components/layout/chat/chatMain.vue
@@ -12,7 +12,7 @@
       </Row>
 
       <Row class="chat-main-warning">
-        <Alert type="warning" >重要信息请尽快保存, 以防丢失</Alert>
+        <Alert type="warning" >7日前信息将自动清除, 重要信息请尽快保存</Alert>
       </Row>
 
       <Row class="chat-main-content">
@@ -26,7 +26,6 @@
                     <Avatar shape="square" icon="ios-person" size="default" />
                     <span class="chat-user-info-name">超级管理员</span>
                   </div>
-
                   <div class="chat-user-info">
                     <Avatar shape="square" icon="ios-person" size="default" />
                     <span class="chat-user-info-name">办案好几十</span>
@@ -56,20 +55,55 @@
             </Collapse>
           </TabPane>
 
-          <TabPane icon="md-contacts">2</TabPane>
+          <TabPane icon="md-contacts">
+            <div class="chat-mian-content-group">
+              <div class="chat-group-info" >
+                <div class="chat-group-img">
+                  <Avatar shape="square" icon="ios-people" size="default" />
+                </div>
+                <div class="chat-group-name">
+                  <a href="#" @click="handleIM">第不知道几次会议交流群</a>
+                </div>
+                <div class="chat-group-option">
+                  <span>创建人: </span>
+                  <span> 超级管理员 </span>
+
+                  <Icon type="ios-trash-outline" size="17" @click="handleIMClear"/>
+                  <Icon type="ios-build" size="17" @click="handleIMSetting"/>
+                </div>
+              </div>
+
+              <div class="chat-group-info">
+                <div class="chat-group-img">
+                  <Avatar shape="square" icon="ios-people" size="default" />
+                </div>
+                <div class="chat-group-name">
+                  <a href="#" @click="handleIM">没事聊一聊</a>
+                </div>
+                <div class="chat-group-option">
+                  <span>创建人: </span>
+                  <span> 里斯 </span>
+
+                  <Icon type="ios-trash-outline" size="17"/>
+                </div>
+              </div>
+            </div>
+          </TabPane>
         </Tabs>
       </Row>
 
-      <Row class="chat-main-footer">
-        <Icon type="md-search" size="20"/>
-        <Icon type="ios-add-circle-outline" size="20"/>
-      </Row>
+      <CChatMainFooter />
     </div>
   </div>
 </template>
 <script>
+import CChatMainFooter from '@/components/layout/chat/chatMainFooter'
+
 export default {
   name: 'CChatMain',
+  components: {
+    CChatMainFooter
+  },
   props: {
     value: {type: Boolean, default: false, required: true}
   },
@@ -85,6 +119,15 @@ export default {
   methods: {
     closeChat () {
       this.isShow = false
+    },
+    handleIM () {
+      console.info('handleIM')
+    },
+    handleIMClear () {
+      console.info('handleIMClear')
+    },
+    handleIMSetting () {
+      console.info('handleIMSetting')
     }
   }
 }
@@ -194,14 +237,60 @@ export default {
           }
         }
       }
-    }
 
-    .chat-main-footer {
-      height: 50px;
-      padding: 0px 20px;
-      .ivu-icon {
-        cursor:pointer;
-        margin-right: 15px;
+      .chat-mian-content-group {
+        .chat-group-info {
+          height: 60px;
+          margin-bottom: 10px;
+          .chat-group-img {
+            float: left;
+            line-height: 60px;
+            .ivu-avatar {
+              width: 40px;
+              height: 40px;
+              font-size: 30px;
+              line-height: 40px;
+              background: #5cadff;
+            }
+          }
+
+          .chat-group-name{
+            margin-left: 10px;
+            height: 30px;
+            float: left;
+            width: 200px;
+            font-size: 17px;
+            line-height: 40px;
+            a{
+              color: #17233d;
+            }
+            a:hover {
+              color: #5cadff;
+            }
+          }
+
+          .chat-group-option{
+            margin-left: 10px;
+            color: #808695;
+            float: left;
+            width: 200px;
+            height: 30px;
+            font-size: 14px;
+            line-height: 30px;
+            .ivu-icon {
+              float: right;
+              line-height: 30px;
+              margin-left: 10px;
+              cursor:pointer;
+            }
+            .ivu-icon:hover {
+              color: #ff9900;
+            }
+          }
+        }
+        .chat-group-info:hover {
+          background: #e8eaec;
+        }
       }
     }
   }
---
+++ src/components/layout/chat/chatMainFooter.vue
@@ -0,0 +1,43 @@
+<template>
+  <Row class="chat-main-footer">
+    <Icon type="md-search" size="20"/>
+    <Icon type="ios-add-circle-outline" size="20"/>
+    <Icon type="ios-trash-outline" size="20" @click="handleDelete"/>
+    <Icon type="ios-information-circle-outline" size="20" @click="handleWarning"/>
+  </Row>
+</template>
+<script>
+export default {
+  name: 'CChatMainFooter',
+  methods: {
+    /**
+     * 提示
+     */
+    handleWarning () {
+      this.$CSure({
+        'content': '重要信息请及时存储！！！',
+        'confirm': () => { }
+      })
+    },
+    /**
+     * 清空聊天记录
+     */
+    handleDelete () {
+      this.$CDelete({
+        'content': '<p>聊天记录将被清空</p><p>是否继续？</p>',
+        'confirm': () => { }
+      })
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .chat-main-footer {
+    height: 50px;
+    padding: 0px 20px;
+    .ivu-icon {
+      cursor:pointer;
+      margin-right: 15px;
+    }
+  }
+</style>
