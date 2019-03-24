59e7e7de4dae881550076758002695581f5045e3
hejiawang <952327407@qq.com>
2019-03-24 19:37:55 +0800

    init

--- src/views/ieg/major/form.vue
+++ src/views/ieg/major/form.vue
@@ -1,5 +1,6 @@
 <template>
-  <Modal v-model="isShow" :title="title" :loading="loading" @on-ok="ok" @on-cancel="cancel" @on-visible-change="visibleChange" width="1000">
+  <Modal v-model="isShow" :title="title" :loading="loading" @on-ok="ok" @on-cancel="cancel"
+         @on-visible-change="visibleChange" width="1200" class="ieg-major-form">
     <Form ref="majorForm" :model="majorForm" :rules="majorRules" :label-width="90">
       <Row> <Alert show-icon>专 业 基 本 信 息 </Alert> </Row>
       <Row  :gutter="32">
@@ -47,6 +48,79 @@
       </Row>
 
       <Row> <Alert show-icon type="warning">专 业 详 细 信 息 </Alert> </Row>
+      <FormItem label="专业简介" prop="describe">
+        <Input type="textarea" v-model.trim="majorForm.describe" :maxlength="500" clearable />
+      </FormItem>
+      <FormItem label="从业方向" prop="workDirection">
+        <Input type="textarea" v-model.trim="majorForm.workDirection" :maxlength="500" clearable />
+      </FormItem>
+      <FormItem label="专业课程" prop="course">
+        <Input type="textarea" v-model.trim="majorForm.course" :maxlength="500" clearable />
+      </FormItem>
+      <Row  :gutter="32">
+        <Col span="6">
+          <FormItem label="男生比例" prop="ratioSexMan">
+            <InputNumber :max="100" :min="0" v-model="majorForm.ratioSexMan"
+                         :formatter="value => `${value}%`"
+                         :parser="value => value.replace('%', '')" style="width: 100%"/>
+          </FormItem>
+        </Col>
+        <Col span="6">
+          <FormItem label="女生比例" prop="ratioSexWoman">
+            <InputNumber :max="100" :min="0" v-model="majorForm.ratioSexWoman"
+                         :formatter="value => `${value}%`"
+                         :parser="value => value.replace('%', '')"  style="width: 100%"/>
+          </FormItem>
+        </Col>
+        <Col span="6">
+          <FormItem label="文科比例" prop="ratioCourseArts">
+            <InputNumber :max="100" :min="0" v-model="majorForm.ratioCourseArts"
+                         :formatter="value => `${value}%`"
+                         :parser="value => value.replace('%', '')"  style="width: 100%"/>
+          </FormItem>
+        </Col>
+        <Col span="6">
+          <FormItem label="理科比例" prop="ratioCourseSci">
+            <InputNumber :max="100" :min="0" v-model="majorForm.ratioCourseSci"
+                         :formatter="value => `${value}%`"
+                         :parser="value => value.replace('%', '')"  style="width: 100%"/>
+          </FormItem>
+        </Col>
+      </Row>
+      <Row  :gutter="32">
+        <Col span="18">
+          <FormItem label="学生规模" prop="studentScope">
+            <Input type="text" v-model.trim="majorForm.studentScope" :maxlength="50" clearable />
+          </FormItem>
+        </Col>
+        <Col span="6">
+          <FormItem label="综合评价" prop="ratioAssessWhole">
+            <Rate allow-half v-model="majorForm.ratioAssessWhole"/>
+          </FormItem>
+        </Col>
+      </Row>
+      <Row  :gutter="32">
+        <Col span="6">
+          <FormItem label="就业评价" prop="ratioAssessWork">
+            <Rate allow-half v-model="majorForm.ratioAssessWork"/>
+          </FormItem>
+        </Col>
+        <Col span="6">
+          <FormItem label="教学评价" prop="ratioAssessStudy">
+            <Rate allow-half v-model="majorForm.ratioAssessStudy"/>
+          </FormItem>
+        </Col>
+        <Col span="6">
+          <FormItem label="办学评价" prop="ratioAssessShool">
+            <Rate allow-half v-model="majorForm.ratioAssessShool"/>
+          </FormItem>
+        </Col>
+        <Col span="6">
+          <FormItem label="难易评价" prop="ratioAssessDifficulty">
+            <Rate allow-half v-model="majorForm.ratioAssessDifficulty"/>
+          </FormItem>
+        </Col>
+      </Row>
 
     </Form>
   </Modal>
@@ -86,15 +160,15 @@ export default {
         workDirection: null,
         course: null,
         studentScope: null,
-        ratioSexMan: 0,
-        ratioSexWoman: 0,
-        ratioCourseArts: 0,
-        ratioCourseSci: 0,
-        ratioAssessWhole: 0,
-        ratioAssessWork: 0,
-        ratioAssessStudy: 0,
-        ratioAssessShool: 0,
-        ratioAssessDifficulty: 0
+        ratioSexMan: 50,
+        ratioSexWoman: 50,
+        ratioCourseArts: 50,
+        ratioCourseSci: 50,
+        ratioAssessWhole: 5,
+        ratioAssessWork: 5,
+        ratioAssessStudy: 5,
+        ratioAssessShool: 5,
+        ratioAssessDifficulty: 5
       },
       majorRules: {
 
@@ -111,3 +185,10 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+  .ieg-major-form {
+    .ivu-modal {
+      top: 10px;
+    }
+  }
+</style>
