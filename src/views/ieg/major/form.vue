b62096fc8ebf5a7d0c7e1099b9de083e5710493b
hejiawang <952327407@qq.com>
2019-03-24 14:15:25 +0800

    init

---
+++ src/views/ieg/major/form.vue
@@ -0,0 +1,113 @@
+<template>
+  <Modal v-model="isShow" :title="title" :loading="loading" @on-ok="ok" @on-cancel="cancel" @on-visible-change="visibleChange" width="1000">
+    <Form ref="majorForm" :model="majorForm" :rules="majorRules" :label-width="90">
+      <Row> <Alert show-icon>专 业 基 本 信 息 </Alert> </Row>
+      <Row  :gutter="32">
+        <Col span="12">
+          <FormItem label="学历层次" prop="degreeType">
+            <RadioGroup v-model="majorForm.degreeType">
+              <Radio label="B">本科</Radio>
+              <Radio label="Z">专科</Radio>
+            </RadioGroup>
+          </FormItem>
+        </Col>
+        <Col span="12">
+          <FormItem label="层级分类" prop="levelType">
+            <RadioGroup v-model="majorForm.levelType">
+              <Radio label="One">门类</Radio>
+              <Radio label="Two">学科</Radio>
+              <Radio label="Three">专业</Radio>
+            </RadioGroup>
+          </FormItem>
+        </Col>
+      </Row>
+      <Row  :gutter="32">
+        <Col span="12">
+          <FormItem label="专业名称" prop="name">
+            <Input type="text" v-model.trim="majorForm.name" :maxlength="50" clearable />
+          </FormItem>
+        </Col>
+        <Col span="12">
+          <FormItem label="专业编码" prop="code">
+            <Input type="text" v-model.trim="majorForm.code" :maxlength="50" clearable />
+          </FormItem>
+        </Col>
+      </Row>
+      <Row  :gutter="32">
+        <Col span="12">
+          <FormItem label="上级专业" prop="parentId">
+            <Input type="text" v-model.trim="majorForm.parentId" :maxlength="50" clearable />
+          </FormItem>
+        </Col>
+        <Col span="12">
+          <FormItem label="专业排序" prop="sort">
+            <InputNumber :max="1000" :min="1" v-model="majorForm.sort" style="width: 100%"/>
+          </FormItem>
+        </Col>
+      </Row>
+
+      <Row> <Alert show-icon type="warning">专 业 详 细 信 息 </Alert> </Row>
+
+    </Form>
+  </Modal>
+</template>
+<script>
+export default {
+  name: 'IegMajor_Form',
+  props: {
+    value: {type: Boolean, default: false, required: true},
+    type: {type: String, default: 'raise', required: true}
+  },
+  watch: {
+    value (val) { this.isShow = val },
+    isShow (val) { this.$emit('input', val) }
+  },
+  computed: {
+    title () {
+      let titleAry = {
+        'modify': '编辑专业信息',
+        'raise': '新增专业信息'
+      }
+      return titleAry[this.type]
+    }
+  },
+  data () {
+    return {
+      loading: true,
+      isShow: false,
+      majorForm: {
+        parentId: null,
+        degreeType: 'B',
+        levelType: 'One',
+        name: null,
+        code: null,
+        sort: 1,
+        describe: null,
+        workDirection: null,
+        course: null,
+        studentScope: null,
+        ratioSexMan: 0,
+        ratioSexWoman: 0,
+        ratioCourseArts: 0,
+        ratioCourseSci: 0,
+        ratioAssessWhole: 0,
+        ratioAssessWork: 0,
+        ratioAssessStudy: 0,
+        ratioAssessShool: 0,
+        ratioAssessDifficulty: 0
+      },
+      majorRules: {
+
+      }
+    }
+  },
+  methods: {
+    ok () {
+    },
+    cancel () {
+    },
+    visibleChange (isOpen) {
+    }
+  }
+}
+</script>
---
+++ src/views/ieg/major/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <Layout v-layoutIn>
+    <Row :gutter="32" class="ieg-major-main">
+      <Col span="6">
+        <Row class="ieg-major-button">
+          <ButtonGroup >
+            <Button type="primary" icon="ios-add-circle-outline" @click="raiseHandle"> 新增 </Button>
+            <Button type="warning" icon="ios-brush-outline"> 修改 </Button>
+            <Button type="error" icon="ios-trash-outline"> 删除 </Button>
+          </ButtonGroup>
+        </Row>
+        <Row class="ieg-major-degree">
+          <Tabs @on-click="selectDegreeHandle">
+            <TabPane label="本科" icon="ios-cube-outline" name="B"/>
+            <TabPane label="专科" icon="md-crop" name="Z"/>
+          </Tabs>
+        </Row>
+
+      </Col>
+      <Col span="18">
+
+      </Col>
+
+      <CIegMajorForm v-model="showForm" :type="formType" />
+    </Row>
+  </Layout>
+</template>
+<script>
+import CIegMajorForm from '@/views/ieg/major/form'
+
+export default {
+  name: 'IegMajor',
+  components: {
+    CIegMajorForm
+  },
+  data () {
+    return {
+      showForm: false,
+      formType: ''
+    }
+  },
+  methods: {
+    selectDegreeHandle (tabName) {
+      console.info(tabName)
+    },
+    raiseHandle () {
+      this.formType = 'raise'; this.showForm = true
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .ieg-major-main{
+    height: 100%;
+    .ieg-major-button{
+      height: 60px;
+    }
+    .ieg-major-degree{
+      height: 60px;
+      .ivu-tabs-nav-container{
+        font-size: 17px;
+      }
+      .ivu-tabs-nav .ivu-tabs-tab .ivu-icon{
+        width: 17px;
+        height: 17px;
+      }
+    }
+  }
+</style>
