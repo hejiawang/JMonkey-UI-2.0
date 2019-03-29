fa2473484e871b0dd58b53dde7b0871327b4c02b
hejiawang <952327407@qq.com>
2019-03-29 17:47:16 +0800

    init

--- src/views/ieg/schoolProblem/index.vue
+++ src/views/ieg/schoolProblem/index.vue
@@ -10,7 +10,17 @@
       </Col>
     </Row>
     <Row class="ieg-school-problem-main">
-
+      <Row v-if="problemList.length === 0" class="ieg-school-problem-none">暂无问题</Row>
+      <Collapse accordion v-else>
+        <Panel v-for="problem in problemList" :name="problem.id" :key="problem.id">
+          {{problem.problem}}
+          <p slot="content">
+            <Row class="answer-body ql-container ql-snow">
+              <Row class="ql-editor" v-html="problem.answer" />
+            </Row>
+          </p>
+        </Panel>
+      </Collapse>
     </Row>
 
     <CIegSchoolProblemForm v-model="showForm" :type="formType" :schoolId="schoolId"
@@ -19,6 +29,10 @@
 </template>
 <script>
 import CIegSchoolProblemForm from '@/views/ieg/schoolProblem/form'
+import { list } from '@/api/ieg/schoolProblem'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 export default {
   name: 'IegSchoolProblem',
@@ -43,16 +57,27 @@ export default {
   },
   data () {
     return {
+      listLoading: false,
       showForm: false,
       formType: '',
-      currentProblem: null
+      currentProblem: null,
+      problemList: []
     }
   },
   created () {
     this.initList()
   },
   methods: {
-    initList () {},
+    /**
+     * init list data
+     */
+    initList () {
+      this.listLoading = true
+      list({schoolId: this.schoolId}).then(data => {
+        this.problemList = data.result
+        this.listLoading = false
+      })
+    },
     /**
      * 返回学院列表页
      */
@@ -73,6 +98,20 @@ export default {
     .ieg-school-problem-main {
       margin-top: 10px;
       height: calc(100% - 55px);
+      overflow-y: auto;
+      .answer-body {
+        height: calc(100% - 110px);
+        overflow-y: auto;
+        border: 0px;
+      }
+      .ivu-collapse-header {
+        font-size: 17px;
+      }
+      .ieg-school-problem-none {
+        font-size: 17px;
+        line-height: 200px;
+        text-align: center;
+      }
     }
   }
 </style>
