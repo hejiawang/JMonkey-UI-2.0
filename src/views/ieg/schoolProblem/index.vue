a6ab2ce2c0e348458be70bb09994697dc437b92a
hejiawang <952327407@qq.com>
2019-03-29 19:57:15 +0800

    init

--- src/views/ieg/schoolProblem/index.vue
+++ src/views/ieg/schoolProblem/index.vue
@@ -14,6 +14,8 @@
       <Collapse accordion v-else>
         <Panel v-for="problem in problemList" :name="problem.id" :key="problem.id">
           {{problem.problem}}
+          <span class="ieg-school-problem-event"><Icon type="ios-trash-outline" @click="deleteHandle(problem)"/></span>
+          <span class="ieg-school-problem-event"><Icon type="ios-brush-outline" @click="modifyHandle(problem)"/></span>
           <p slot="content">
             <Row class="answer-body ql-container ql-snow">
               <Row class="ql-editor" v-html="problem.answer" />
@@ -24,12 +26,12 @@
     </Row>
 
     <CIegSchoolProblemForm v-model="showForm" :type="formType" :schoolId="schoolId"
-                           :faculty="currentProblem" @refresh="initList"/>
+                           :problem="currentProblem" @refresh="initList"/>
   </Layout>
 </template>
 <script>
 import CIegSchoolProblemForm from '@/views/ieg/schoolProblem/form'
-import { list } from '@/api/ieg/schoolProblem'
+import { list, del } from '@/api/ieg/schoolProblem'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
@@ -88,7 +90,31 @@ export default {
      * raise school problem handle
      */
     raiseHandle () {
-      this.formType = 'raise'; this.showForm = true
+      this.currentProblem = null; this.formType = 'raise'; this.showForm = true
+    },
+    /**
+     * 删除院校常见问题
+     * @param problem 常见问题
+     */
+    deleteHandle (problem) {
+      this.$CDelete({
+        'content': '<p>问题： <span style="color: #f60">' + problem.problem + '</span> 将被删除</p><p>是否继续？</p>',
+        'confirm': () => {
+          del(problem.id).then((data) => {
+            this.initList()
+
+            if (data.result) this.$Message.success('删除成功')
+            else this.$Message.error('删除失败')
+          })
+        }
+      })
+    },
+    /**
+     * 修改院校常见问题
+     * @param problem 常见问题
+     */
+    modifyHandle (problem) {
+      this.currentProblem = problem; this.formType = 'modify'; this.showForm = true
     }
   }
 }
@@ -111,6 +137,10 @@ export default {
         font-size: 17px;
         line-height: 200px;
         text-align: center;
+      }
+      .ieg-school-problem-event {
+        float: right;
+        margin-right: 20px;
       }
     }
   }
